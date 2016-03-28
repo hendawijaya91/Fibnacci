8d828b27e5bd727bd342810fca14afffceff0d6d
hendawijaya91 <hendawijaya91@gmail.com>
2016-03-28 21:53:03 +0700

    asds
    asdas

--- fibonacci/src/fibonacci/Fibonacci.java
+++ fibonacci/src/fibonacci/Fibonacci.java
@@ -10,16 +10,18 @@ public class Fibonacci {
         System.out.println("Masukan Jumah Angka: ");
         angka= s.nextInt();
         
-     int n1=1,n2=1,n3,i,count=angka;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<angka;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }
-        System.out.println("");
-}
-}
+  int n1 = 1, n2 = 1, n3, i, count = angka;
+        System.out.print(n1 + " " + n2);//printing 0 and 1    
+
+        for (i = 2; i < angka; ++i)//loop starts from 2 because 0 and 1 are already printed    
+        {
+            n3 = n1 + n2;
+            System.out.print(" " + n3 );
+            n1 = n2;
+            n2 = n3;
+            sum = sum + n3; 
+        }
+        sum = sum+2;
+        System.out.println("\nJumlah : " + sum);
+    }
+
